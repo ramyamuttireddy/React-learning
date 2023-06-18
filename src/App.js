574555207a477f294e6935f65847280041fc5f77
Ramya <muttireddyramya9@gmail.com>
2023-06-18 16:37:42 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Counters from './components/counters/counter';
+import DropDown from './components/dropdowns/dropsown';
+import './components/style.css';
+import Refrences from './components/refrences/refrences';
+import Form from './components/froms/from';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <Counters />
+       <DropDown />
+       <Refrences />
+       <Form />
+       
       </header>
     </div>
   );
