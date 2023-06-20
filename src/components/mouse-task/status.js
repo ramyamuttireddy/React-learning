51e006cd573a54f7203ebbe9a258620847a93357
Ramya <muttireddyramya9@gmail.com>
2023-06-20 14:10:10 +0530

    css completed in mouse ove task

--- src/components/mouse-task/status.js
+++ src/components/mouse-task/status.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Status({hover , moving}) {
   return (
     <div>
-      <div> Mouse situvation {hover ? 'mouse enterd' : 'mouse left '}</div>
+      <div className='mouse-text'> {hover ? 'Mouse Enterd' : 'Mouse Left '}</div>
       
     </div>
   )
