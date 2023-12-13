26fd3c0f5a91cfe4d927d2c6d171c40c417ef0f0
Ramya <muttireddyramya9@gmail.com>
2023-12-13 14:54:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import './App.css';
 import Button from './components/Buttons/Button';
- 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a> */}
-        <Button text="Read more"  bgColor="primary"  buttonType="solid"/>
-        <Button text="Read more"  bgColor="secondary"  buttonType="solid"/>
-        <Button text="Read more"  bgColor="white"  buttonType="solid"/>
-        <Button text="Read more"  bgColor="black"  buttonType="solid"/>
+        <Button text="Read More" bgColor="primary" buttonType="solid" />
+        <Button text="Read More" bgColor="secondary" buttonType="solid" />
+        <Button text="Read More" bgColor="black" buttonType="solid" />
+        <Button text="Read More" bgColor="white" buttonType="solid" />
 
-        <Button text="Read more"  bgColor="primary"  buttonType="outline"/>
-        <Button text="Read more"  bgColor="secondary"  buttonType="outline"/>
-        <Button text="Read more"  bgColor="white"  buttonType="outline"/>
-        <Button text="Read more"  bgColor="black"  buttonType="outline"/>
 
-      </header>
-
-   
-    </div>
+        <Button text="Read More" bgColor="primary" buttonType="outline" />
+        <Button text="Read More" bgColor="secondary" buttonType="outline" />
+        <Button text="Read More" bgColor="black" buttonType="outline" />
+        <Button text="Read More" bgColor="white" buttonType="outline" />
+     </header>
+</div>
   );
 }
 
--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,10 +1,11 @@
  import './button.scss';
+ import React from 'react'
  
-
-function Button(props){
-  
-  return(
-     <button className={`${props.bgColor ? props.bgColor : ""}  ${props.buttonType ? props.buttonType : ""}`}>{props.text}</button>
-  )
-}
-export default Button;
+ function Button(props) {
+   return (
+     <div>
+       <button className={`${props.bgColor? props.bgColor:""} ${props.buttonType ? props.buttonType :""}`}> {props.text} </button>
+     </div>
+   )
+ }
+ export default Button;
